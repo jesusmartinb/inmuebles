bd05826456282594632ed2674b9a07b919ee1e17
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2023-04-08 15:04:48 +0200

    Se añade Express JS y se crea server

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log('Escuchando en el puerto: ' + port))
