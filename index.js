42c6e666d13e1f7b3a953a619420ec9cc964c31c
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2023-04-08 15:54:10 +0200

    Se crean el archivo de modelo y de rutas de inmuebles, se instala mongoose y se conecta a MongoDB

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 
@@ -6,3 +7,7 @@ app.use(express.json())
 const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log('Escuchando en el puerto: ' + port))
+
+mongoose.connect('mongodb://127.0.0.1:27017/inmueblesUnir')
+	.then(() => console.log('Conectado a MongoDB'))
+	.catch((error) => console.log('No se ha conectado a MongoDB'))
