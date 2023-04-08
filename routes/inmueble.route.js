b35cb27f599257aadbabafd4cd25dc945ac1eeae
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2023-04-08 20:02:54 +0200

    Se instala el paquete express-validator. Se implementa en endpoint para método POST

--- routes/inmueble.route.js
+++ routes/inmueble.route.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const Inmueble = require('../models/inmueble.model')
 const router = express.Router()
+const { body, validationResult } = require('express-validator');
 
 router.get('/', async (req, res) => {
 	const inmuebles = await Inmueble.find()
@@ -13,5 +14,58 @@ router.get('/:id', async (req, res) => {
 
 	res.send(inmueble)
 })
+
+router.post(
+	'/',
+	body('piso')
+		.isInt()
+		.notEmpty().withMessage('Por favor introduzca el número de piso')
+		.trim()
+		.isLength({ min: 1, max: 1 }).withMessage('Escriba un número entre 1 y 4'),
+	body('letra')
+		.isString().withMessage('Escriba letras de la A a la D')
+		.notEmpty()
+		.trim()
+		.isLength({ min: 1, max: 1 }).withMessage('Escoja entre A B C D'),
+	body('extension')
+		.isInt()
+		.notEmpty().withMessage('Por favor introduzca un valor númerico')
+		.isLength({ min: 60, max: 100 }).withMessage('Introduzca un valor entre 60 y 100'),
+	body('habitaciones')
+		.isInt()
+		.notEmpty()
+		.trim()
+		.isLength({ min: 1, max: 1 }).withMessage('Escriba un número entre 2 y 4'),
+	body('alquilado')
+		.isBoolean().withMessage('Por favor escriba "yes" o "no"'),
+	body('nombrePropietario')
+		.isString()
+		.notEmpty().withMessage('Escriba el nombre del propietario')
+		.toLowerCase()
+		.trim()
+		.isLength({ min: 2, max: 80 }).withMessage('El nombre ha de estar entre 2 y 80 caracteres'),
+	body('emailContacto')
+		.trim()
+		.notEmpty().withMessage('El campo email es obligatorio')
+		.isEmail().withMessage('Por favor introduzca una dirección de email valida'),
+	async (req, res) => {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			return res.status(422).json({ errors: errors.array() })
+		}
+
+		const inmueble = new Inmueble({
+			piso: req.body.piso,
+			letra: req.body.letra,
+			extension: req.body.extension,
+			habitaciones: req.body.habitaciones,
+			alquilado: req.body.alquilado,
+			nombrePropietario: req.body.nombrePropietario,
+			emailContacto: req.body.emailContacto
+		})
+
+		const resultado = await inmueble.save()
+		res.status(201).send(resultado)
+	})
 
 module.exports = router
