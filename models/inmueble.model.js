4af5974bb13e1aa234e1348e4bb61a4b779da706
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2023-04-08 16:39:41 +0200

    Se crea el modelo de inmuebles

---
+++ models/inmueble.model.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+const inmuebleSchema = new mongoose.Schema({
+	piso: {
+		type: Number,
+		required: true,
+		min: 1,
+		max: 4
+	},
+	letra: {
+		type: String,
+		required: true,
+		uppercase: true,
+		trim: true,
+		minLength: 1,
+		maxLength: 1,
+		enum: ["A", "B", "C", "D"]
+	},
+	extension: {
+		type: Number,
+		required: true,
+		min: 60,
+		max: 100,
+	},
+	habitaciones: {
+		type: Number,
+		required: true,
+		min: 2,
+		max: 4,
+		enum: [2, 3, 4]
+	},
+	alquilado: {
+		type: Boolean,
+	},
+	nombrePropietario: {
+		type: String,
+		required: true,
+		lowercase: true,
+		trim: true,
+		minLength: 2,
+		maxLength: 80
+	},
+	emailContacto: {
+		type: String,
+		required: true,
+		lowercase: true,
+		trim: true,
+		maxLength: 100
+	}
+})
+
+const Inmueble = mongoose.model('inmueble', inmuebleSchema)
+
+module.exports = Inmueble
